ac405343591dd5152c97855dd17cac4a2e840a32
Magusy2020 <a20221972@pucp.edu.pe>
2024-11-28 21:24:10 -0500

    Update PC4_plantilla_blog.py

--- PC4_plantilla_blog.py
+++ PC4_plantilla_blog.py
@@ -140,12 +140,14 @@ st.markdown("<h2 style='text-align: center;'>Explicación de dos temas de las cl
 # unsafe_allow_html=True: Este es un argumento opcional en la función markdown.
 
 # Agregamos un video a la aplicación web ( menor a 20 MB)
-st.video("PC2_video.mp4")
-st.video("PC3_video.mp4")
+# st.video("PC2_video.mp4")
+# st.video("PC3_video.mp4")
 # st.video("ppc-2024-1.mp4"): Esta línea está agregando un video a la aplicación web.
 # Agregamos un enlace a la página web donde está el video.
-enlace = f'<a href="https://drive.google.com/drive/folders/1rHCmbRaaW6G1MY_0Ud2Gv82AKSJDzI-1?usp=drive_link" target="_blank"><button>Si te gusta el chisme dale click></a>'
+enlace = f'<a href="https://drive.google.com/file/d/1IlTW1-I-iuihIxw3yW0gKSesvcNxYw6H/view?usp=drive_link" target="_blank"><button>Si te gusta el chisme dale click></a>'
 st.markdown(enlace, unsafe_allow_html=True)
+enlace_2 = f'<a href="https://drive.google.com/file/d/1zHQTeBIx05oQXzkU9sarnJuOfXOFbko1/view?usp=drive_link" target="_blank"><button>Si te gusta el chisme dale click></a>'
+st.markdown(enlace_2, unsafe_allow_html=True)
 # f'<a href="URL" target="_blank"><button>Nombre</button></a>':
 # La etiqueta <a> se utiliza para crear un enlace en HTML.
 # El atributo href se utiliza para especificar la URL de destino del enlace.
